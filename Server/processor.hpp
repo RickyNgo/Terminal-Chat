7a5358bf681261eb11a9a404eff55063bd112f38
palmebri <palmebri@oregonstate.edu>
2017-11-16 19:11:06 -0500

    Login Functionality Works
    Two clients are prevented from using the same alias. Need to find a way
    to remove connections/operations when a user quits.

--- Server/processor.hpp
+++ Server/processor.hpp
@@ -1,6 +1,8 @@
 #ifndef __PROCESSOR_HPP__
 #define __PROCESSOR_HPP__
 
+#include <boost/system/error_code.hpp>
+using boost::system::error_code;
 #include <boost/asio.hpp>
 using boost::asio::io_service;
 #include <boost/asio/buffer.hpp>
@@ -13,32 +15,61 @@ using boost::asio::io_service;
 #include <boost/thread.hpp>
 #include <boost/thread/recursive_mutex.hpp>
 #include <boost/thread/condition_variable.hpp>
+using boost::condition_variable;
 #include <iostream>
+#include <string>
 #include <queue>
-#include <list>
 #include <map>
 #include "guest.hpp"
-// #include "channel.hpp"
+#include "channel.hpp" 
 
 class Connection;
 
-typedef boost::system::error_code						error_code;
+typedef boost::asio::mutable_buffers_1				mutable_buffer;
+typedef boost::asio::const_buffers_1				const_buffer;
 
 class Processor :
 private boost::noncopyable {
 public:
 
-	typedef boost::asio::mutable_buffers_1			mutable_buffer;
-	typedef boost::function<error_code( void )> 	do_async_op;
-	typedef boost::function<void( error_code )> 	on_async_op;
-	typedef boost::condition_variable				condition;
-	typedef std::pair<const unsigned char *,
-		Guest::pointer>								guest;
+	typedef boost::function<error_code( void )> 		do_async_op;
+	typedef boost::function<void( error_code )> 		on_async_op;
 
-	/* Start Operation Class */
+	Processor( io_service & ios );
+	~Processor( void );
 
-	#ifndef __OPERATION__
-	#define __OPERATION__
+	/* Commands */
+
+	void 	async_stage( Guest::pointer, on_async_op );
+	void	async_login( Guest::pointer, const_buffer, on_async_op );
+	// void	async_leave( Guest::pointer, on_async_op );
+
+
+private:
+
+
+	typedef boost::recursive_mutex::scoped_lock			scoped_lock;
+	typedef std::pair<const char *, Guest::pointer>		guest_t;
+	typedef std::pair<const char *, Channel>			channel_t;
+
+	/* Start Error Code Category */
+
+#ifndef __ERROR__
+#define __ERROR__
+
+	class Error :
+	public boost::system::error_category {
+	public:
+ 		const char * 	name( void ) const noexcept;
+  		std::string 	message( int ev ) const;
+	};
+
+#endif
+
+/* Start Operation Class */
+
+#ifndef __OPERATION__
+#define __OPERATION__
 
 	class Operation :
 	public boost::enable_shared_from_this<Processor::Operation> {
@@ -60,36 +91,40 @@ public:
 
 	};
 
-	#endif 
+#endif 
 
-	/* End Operation Class */
+/* End Operation Class */
 
-	Processor( io_service & ios );
-	~Processor( void );
+#ifndef __COMPARATOR_EQ__
+#define __COMPARATOR_EQ__
 
-	/* Commands */
+class Equal {
+public:
+	bool operator() ( const char *, const char * ) const noexcept;
+};
 
-	void 	async_stage( Guest::pointer, on_async_op );
-	void	async_login( mutable_buffer, on_async_op );
+#endif
 
-private:
-	io_service &										ios_;
-	std::vector<Guest::pointer>							stage_;
-	boost::recursive_mutex								stage_m_;
-	std::map<const unsigned char *, Guest::pointer>		guests_;
-	// std::set<Channel::pointer>						channels_;
-	std::queue<Processor::Operation> 					ops_;
-	boost::mutex 										ops_m_;
-	condition 											cond_;
-	boost::thread_group									threads_;
-	const std::uint8_t  								num_t_;					
+	Error 													proc_errc_;
+	io_service &											ios_;
+	std::set<Guest::pointer>								stage_;
+	boost::recursive_mutex									stage_m_;
+	std::map<const char *, Guest::pointer, Equal>			guests_;
+	boost::recursive_mutex									guests_m_;
+	std::map<const char *, Channel, Equal>					channels_;
+	boost::recursive_mutex									channels_m_;
+	std::queue<Processor::Operation> 						ops_;
+	boost::mutex 											ops_m_;
+	condition_variable 										cond_;
+	boost::thread_group										threads_;
+	const std::uint8_t  									num_t_;					
 
 	void			add_( do_async_op, on_async_op );
 	void 			run_( void );
 
-	error_code		do_login_( mutable_buffer );
 	error_code		do_stage_( Guest::pointer );
-
+	error_code		do_login_( Guest::pointer, const_buffer );
+	error_code 		do_create_channel_( Guest::pointer, mutable_buffer );
 };
 
 #endif  /* End Processor */
